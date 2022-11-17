f158bc9c42647f7c7b3855560c1c245ec279da1c
Reinapepeada <abiarrieta@uade.edu.ar>
2022-11-17 12:05:59 -0300

    automatización de CP LOGIN

---
+++ CP_-_PruebaLoginFallida.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+PATH= "C:\chromedriver.exe"
+driver=webdriver.Chrome(PATH)
+
+driver.get('https://magento.softwaretestingboard.com/')
+title=driver.title
+
+time.sleep(3)
+element=driver.find_element(By.LINK_TEXT,'Sign In')
+element.click()
+form2=driver.find_element(By.ID,'email')
+form2.send_keys('ronicost@example.com')
+form1=driver.find_element(By.ID,'pass')
+form1.send_keys('roni_cost3@example.com')
+time.sleep(3)
+form1.send_keys(Keys.RETURN)
